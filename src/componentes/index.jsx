530e141ed249a722e49e3727663ae262fadc5c5e
Avcm00 <cuevaariel009@outlook.com>
2024-06-26 19:27:57 -0500

    html,css login

---
+++ src/componentes/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function principalPage(){
+  return(
+    <div className="paginaP">
+      <div className="barraN">
+        <header>
+          <nav>
+            <ul>
+              <li className="icono1"><a href="#">Inicio</a></li>
+              <li className="icono2"><a href="#">Deportes</a></li>
+              <li className="icono3"><a href="#">Tecnologia</a></li>
+              <li className="icono4"><a href="#">Viajes</a></li>
+              <li className="icono5"><a href="#">Educacion</a></li>
+              <li className="icono6"><a href="#">Entretenimiento</a></li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+      <div className="contenido">
+        <div className="objeto">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+export default principalPage
--- src/componentes/login.js
+++ src/componentes/login.js
@@ -1,28 +1,31 @@
-import React from "react";
-
+import React from 'react';
+import '../Styles/login.css';
 
 function Loginstart() {
   return (
     <div className="box-login">
-      
-        <h1 className="headtext">Iniciar Sesión</h1>
-        <p className="text">Ingresa tus credenciales para acceder a tu cuenta.</p>
+      <h1 className="headtext">Iniciar Sesión</h1>
+      <p className="text">Ingresa tus credenciales para acceder a tu cuenta.</p>
       
       <div className="box-inputs">
-        <div className="username">
+        <div className="input-group">
           <label htmlFor="username">Nombre de usuario</label>
-          <input className="usernamei" placeholder="Ingresa tu nombre de usuario" />
+          <input className="input-field" id="username" placeholder="Ingresa tu nombre de usuario" />
         </div>
-        <div className="password">
+        <div className="input-group">
           <label htmlFor="password">Contraseña</label>
-          <input className="passwordi" type="password" placeholder="Ingresa tu contraseña" />
+          <input className="input-field" type="password" id="password" placeholder="Ingresa tu contraseña" />
         </div>
       </div>
+      
       <div className="footer">
-          <button className="boton">Iniciar Sesión</button>
+        <button className="button">Iniciar Sesión</button>
+      </div>
+      <div>
+        <a href="/register">¿No tienes una cuenta? Regístrate</a>
       </div>
     </div>
-  )
-    
+  );
 }
-export default Loginstart
+
+export default Loginstart;
